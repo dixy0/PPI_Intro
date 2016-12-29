89115d8acdbaefb8738d7d340f66112aec055f2e
Xin Di <synge.x.d@gmail.com>
2016-12-29 17:27:52 -0500

    Update Interaction_simple.m

--- Interaction_simple.m
+++ Interaction_simple.m
@@ -31,6 +31,35 @@ figure; scatter(x_iq,y_volume,25,gender,'filled');
 [r_female,p_female] = corr(x_iq_female,y_volume_female)
 
 
-% Part3 Different methods to examine correlation differences
+% Part3 Different methods to examine correlation (slope) differences 
+design_matrix1 = [[x_iq_male;zeros(100,1)],[zeros(100,1);x_iq_female],gender]; % omited constant term because regstats will be used
+design_matrix2 = [x_iq,gender,x_iq.*gender];
 
+figure; subplot(1,2,1);imagesc(design_matrix1)
+subplot(1,2,2);imagesc(design_matrix2);colormap('gray')
+
+s = regstats(y_volume,design_matrix1);
+[s.tstat.beta, s.tstat.se, s.tstat.t, s.tstat.pval];
+[p,F] = linhyptest(s.beta, s.covb, 0, [0 1 -1 0], s.tstat.dfe)
+
+s = regstats(y_volume,design_matrix2);
+[s.tstat.t(4), s.tstat.pval(4)]
+
+
+% Part 4 differences of centering
+design_matrix3 = [x_iq,gender,x_iq.*detrend(gender,'constant')];
+
+figure; subplot(1,2,1);imagesc(design_matrix2)
+subplot(1,2,2);imagesc(design_matrix3);colormap('gray')
+
+figure; subplot(1,3,1);scatter(x_iq,y_volume,25,gender,'filled');
+subplot(1,3,2);scatter(x_iq,design_matrix2(:,3), 25,gender,'filled');
+subplot(1,3,3);scatter(x_iq,design_matrix3(:,3), 25,gender,'filled');
+
+
+s = regstats(y_volume,design_matrix2);
+[s.tstat.t(4), s.tstat.pval(4)]
+
+s = regstats(y_volume,design_matrix3);
+[s.tstat.t(4), s.tstat.pval(4)]
 
