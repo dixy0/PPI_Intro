a4a04c658f2bf31ca4e63ec4d1656e79da9fe2d7
Xin Di <synge.x.d@gmail.com>
2017-05-29 12:15:31 -0400

    The effect of lamda

--- Deconvolution.m
+++ Deconvolution.m
@@ -9,7 +9,7 @@ clear
 N = 300;
 n = (0:N-1)';                               % n : discrete-time index
 
-x = randn(N,1)                 % neural signal
+x = randn(N,1);                 % neural signal
 
 % hrf = spm_hrf(2);            % define hemodynamic response function from SPM
 hrf = [0;0.0865660809936357;0.374888236471690;0.384923381745461;0.216117315646557;0.0768695652550848;0.00162017719800089;-0.0306078117340448;-0.0373060781329993;-0.0308373715988730;-0.0205161333521205;-0.0116441637490611;-0.00582063147182588;-0.00261854249818620;-0.00107732374408556;-0.000410443522357321;-0.000146257506876445;];
@@ -17,15 +17,14 @@ hrf = [0;0.0865660809936357;0.374888236471690;0.384923381745461;0.21611731564655
 y = conv(hrf, x);
 y = y(1:N);                                 % y : output signal (noise-free)
 
-yn = y + 0.2 * randn(N, 1);                 % yn : output signal (noisy)
+noise = randn(N, 1);
+yn = y + 1 * noise;                 % yn : output signal (noisy)
 
 figure(1)
-subplot(2, 1, 1); plot(x); title('Input signal');
-subplot(2, 1, 2); plot(hrf); title('Impulse response');
-
-figure(2)
-subplot(2, 1, 1); plot(y); title('Output signal');
-subplot(2, 1, 2); plot(yn); title('Output signal (noisy)');
+subplot(2, 2, 1); plot(x); title('Input signal');
+subplot(2, 2, 2); plot(hrf); title('Impulse response');
+subplot(2, 2, 3); plot(y); title('Output signal');
+subplot(2, 2, 4); plot(yn); title('Output signal (noisy)');
 
 
 % Part 2 deconvolution
@@ -44,8 +43,8 @@ g = H \ y;
 lam = 0.1;
 g = (H'*H + lam * eye(N)) \ (H' * y);
 
-figure(3)
-subplot(2, 1, 1); plot(y); title('Output signal (noise-free)');
+figure(2)
+subplot(2, 1, 1); plot(x); title('Input signal');
 subplot(2, 1, 2); plot(g); title(sprintf('Deconvolution (diagonal loading, \\lambda = %.2f)', lam));
 
 % Diagonal loading (noisy)
@@ -53,9 +52,15 @@ lam = 0.1;
 lam = 1;
 g = (H'*H + lam * eye(N)) \ (H' * yn);
 
-figure(4)
-subplot(2, 1, 1); plot(yn); title('Output signal (noise-free)');
+figure(3)
+subplot(2, 1, 1); plot(x); title('Input signal');
 subplot(2, 1, 2); plot(g); title(sprintf('Deconvolution (diagonal loading, \\lambda = %.2f)', lam));
 
+lamr = 0.01:0.01:2;
+for lami = 1:length(lamr)
+    g = (H'*H + lamr(lami) * eye(N)) \ (H' * yn);
+    corr_cov(lami,1) = corr2(x,g);
+    corr_noise(lami,1) = corr2(g-x,noise);
+end
 
 
